4a2b2260ad9bf719741c972ff1ed99f5b8496c4e
vagusx <vagusxl@gmail.com>
2022-02-11 12:46:06 +0800

    fix: change publicPath to make preview works

--- .umirc.ts
+++ .umirc.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   outputPath: '.doc',
   exportStatic: {},
-  base: '/segmented/',
-  publicPath: '/segmented/',
+  base: '/',
+  publicPath: '/',
   hash: true,
   styles: [
     `
