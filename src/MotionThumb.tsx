5d7fe201f3598158cbda20ba7068d0c2e650869e
Redjue <pingfj77@gmail.com>
2023-02-06 11:18:58 +0800

    fix: test cover problem

--- src/MotionThumb.tsx
+++ src/MotionThumb.tsx
@@ -29,7 +29,7 @@ const calcThumbStyle = (
     ? {
         left: targetElement.offsetLeft,
         right:
-          (targetElement.parentElement?.clientWidth as number) -
+          (targetElement.parentElement!.clientWidth as number) -
           targetElement.clientWidth -
           targetElement.offsetLeft,
         width: targetElement.clientWidth,
