792c6dc69e48f2c9785ac5e968bfcbe724792dda
wuxh <wxh1220@gmail.com>
2023-12-07 22:13:32 +0800

    docs: update demo

--- docs/demo/basic.tsx
+++ docs/demo/basic.tsx
@@ -1,14 +1,23 @@
-import '../../assets/style.less';
-import React from 'react';
 import Segmented from 'rc-segmented';
+import React from 'react';
+import '../../assets/style.less';
+
+enum Platform {
+  iOS = 'iOS',
+  Android = 'Android',
+  Web = 'Web',
+}
 
 export default function App() {
+  const [platform, setPlatform] = React.useState<Platform>(Platform.iOS);
+
   return (
     <div>
       <div className="wrapper">
         <Segmented
+          value={platform}
           options={['iOS', 'Android', 'Web']}
-          onChange={(value) => console.log(value, typeof value)}
+          onChange={setPlatform}
         />
       </div>
       <div className="wrapper">
