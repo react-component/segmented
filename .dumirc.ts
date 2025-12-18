4ceb3b40682c1e874e5afe3370580e4e30bfd19b
aojunhao123 <1844749591@qq.com>
2025-12-18 11:37:06 +0800

    fix: remove duplicate radio role and optimize aria attributes

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,18 +1,20 @@
 import { defineConfig } from 'dumi';
+import path from 'path';
 
 const basePath = process.env.GH_PAGES ? '/segmented/' : '/';
 const publicPath = process.env.GH_PAGES ? '/segmented/' : '/';
 
 export default defineConfig({
-  favicons: [
-    'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
-  ],
+  favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
   themeConfig: {
     name: 'Segmented',
-    logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'
+    logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   },
   outputPath: '.doc',
   exportStatic: {},
   base: basePath,
   publicPath,
+  alias: {
+    'rc-segmented': path.resolve(__dirname, 'src/index.tsx'),
+  },
 });
