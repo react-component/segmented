a99e55b25dbafb3143be5382acd378999f7ef52c
afc163 <afc163@gmail.com>
2026-06-30 10:51:00 +0800

    chore: use testing-library dom events

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,4 +1,5 @@
-import { act, fireEvent, render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/dom';
+import { act, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 import Segmented from '../src';
