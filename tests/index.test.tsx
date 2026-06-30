b52818268c155f84faeb888e420bc481e0383d64
afc163 <afc163@gmail.com>
2026-06-30 11:16:07 +0800

    test: keep react testing event behavior

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,5 +1,4 @@
-import { fireEvent } from '@testing-library/dom';
-import { act, render } from '@testing-library/react';
+import { act, fireEvent, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 import Segmented from '../src';
