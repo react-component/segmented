31e63af1ad5acc1b25f5012c6bd32099ffeb7836
afc163 <afc163@gmail.com>
2026-07-01 13:13:46 +0800

    chore: enable strict type checking

--- docs/demo/refs.tsx
+++ docs/demo/refs.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import '../../assets/style.less';
 
 class ClassComponentWithStringRef extends React.Component {
-  refs: {
+  refs!: {
     segmentedRef: HTMLDivElement;
   };
 
